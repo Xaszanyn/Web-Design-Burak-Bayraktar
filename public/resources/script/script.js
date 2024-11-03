a7bedd5f422fb8ff06ea40e8b2f3fdea25b5a5cc
Ekin Aslan <ekinaslan.js@gmail.com>
2024-11-03 21:55:53 +0300

    Before & After

--- public/resources/script/script.js
+++ public/resources/script/script.js
@@ -7,38 +7,8 @@ const galleryRight = document.querySelector("#masonry-right");
 var slide = 0;
 var maximum = 4;
 
-window.addEventListener("scroll", () => {
-  let ratio = Math.ceil((Math.max(0, window.scrollY) / (document.body.offsetHeight - window.innerHeight)) * 100);
-
-  if (!ratio) document.body.classList.add("initial");
-  else document.body.classList.remove("initial");
-
-  if (ratio <= 17) {
-    active.classList.remove("active");
-    active = navigation[0];
-    active.classList.add("active");
-  } else if (ratio <= 32) {
-    active.classList.remove("active");
-    active = navigation[1];
-    active.classList.add("active");
-  } else if (ratio <= 50) {
-    active.classList.remove("active");
-    active = navigation[2];
-    active.classList.add("active");
-  } else if (ratio <= 65) {
-    active.classList.remove("active");
-    active = navigation[3];
-    active.classList.add("active");
-  } else if (ratio <= 85) {
-    active.classList.remove("active");
-    active = navigation[4];
-    active.classList.add("active");
-  } else {
-    active.classList.remove("active");
-    active = navigation[5];
-    active.classList.add("active");
-  }
-});
+const beforeAfters = document.querySelectorAll(".before-after");
+const beforeAfterButtons = document.querySelectorAll(".before-after i");
 
 function clamp(num, min, max) {
   return num < min ? min : num > max ? max : num;
@@ -59,6 +29,70 @@ function moveGallery(direction) {
   }
 
   gallery[0].style.transform = `translateX(-${slide * 15}rem)`;
-  setTimeout(() => (gallery[1].style.transform = `translateX(-${slide * 15}rem)`), 100);
-  setTimeout(() => (gallery[2].style.transform = `translateX(-${slide * 15}rem)`), 200);
+  setTimeout(
+    () => (gallery[1].style.transform = `translateX(-${slide * 15}rem)`),
+    100
+  );
+  setTimeout(
+    () => (gallery[2].style.transform = `translateX(-${slide * 15}rem)`),
+    200
+  );
 }
+
+function handleBeforeAfters() {
+  beforeAfterButtons.forEach((beforeAfterButton) =>
+    beforeAfterButton.addEventListener("click", () => {
+      if (!beforeAfterButton.parentElement.dataset.manuel)
+        beforeAfterButton.parentElement.dataset.manuel = "true";
+
+      renderBeforeAfter(
+        beforeAfterButton.parentElement,
+        beforeAfterButton.parentElement.dataset.phase
+      );
+    })
+  );
+  beforeAfters.forEach((beforeAfter) => {
+    beforeAfter.dataset.phase = 1;
+    beforeAfter.children[0].classList.add("active");
+
+    setInterval(() => {
+      if (beforeAfter.dataset.manuel) return;
+      renderBeforeAfter(beforeAfter, parseInt(beforeAfter.dataset.phase));
+    }, 2000);
+  });
+
+  function renderBeforeAfter(beforeAfter, phase) {
+    for (let index = 1; index < beforeAfter.children.length - 1; index++) {
+      if (index <= phase) beforeAfter.children[index].classList.add("active");
+      else beforeAfter.children[index].classList.remove("active");
+    }
+
+    phase++;
+    beforeAfter.dataset.phase =
+      phase == beforeAfter.children.length - 1 ? 0 : phase;
+  }
+}
+
+window.addEventListener("scroll", () => {
+  let ratio = Math.ceil(
+    (Math.max(0, window.scrollY) /
+      (document.body.offsetHeight - window.innerHeight)) *
+      100
+  );
+
+  if (!ratio) document.body.classList.add("initial");
+  else document.body.classList.remove("initial");
+
+  active.classList.remove("active");
+
+  if (ratio < 12) active = navigation[0];
+  else if (ratio < 23) active = navigation[1];
+  else if (ratio < 44) active = navigation[2];
+  else if (ratio < 67) active = navigation[3];
+  else if (ratio <= 91) active = navigation[4];
+  else active = navigation[5];
+
+  active.classList.add("active");
+});
+
+handleBeforeAfters();
